a52725aaee25b24b9feaf3d1802ce1952177a596
GitHub Action <action@github.com>
2025-03-01 16:08:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/wildwindxy/projectstructureexporter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wildwindxy/projectstructureexporter/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wildwindxy.projectstructureexporter.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wildwindxy.projectstructureexporter.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
